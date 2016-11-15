c4b93636f174a95afd86cba763a1e2b442da93eb
Vincent Lo <vincentsylo@gmail.com>
2016-11-15 23:31:25 +1100

    Replace bcrypt with bcryptjs and setup yarn

--- src/server/api/user.js
+++ src/server/api/user.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import models from '../models';
 
 module.exports = (server) => {
--- src/server/passport.js
+++ src/server/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const models = require('./models');
 
 module.exports = (server) => {
